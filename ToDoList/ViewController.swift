c40d539ffd153035617ea8340fcfc2de8197a5f8
InnaStepanova <inna-panichkina@yandex.ru>
2023-06-13 11:29:40 +0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Лаванда on 13.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

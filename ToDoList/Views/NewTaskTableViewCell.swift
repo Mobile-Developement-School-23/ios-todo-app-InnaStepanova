68129ce47a3420dccdfb1f554aff17f10d0cc207
InnaStepanova <inna-panichkina@yandex.ru>
2023-06-29 15:49:36 +0300

    Добавила боковые свайпы и исправила логику баг с клавиатурой у DetailViewController

---
+++ ToDoList/Views/NewTaskTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewTaskTableViewCell.swift
+//  ToDoList
+//
+//  Created by Лаванда on 29.06.2023.
+//
+
+import UIKit
+
+class NewTaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

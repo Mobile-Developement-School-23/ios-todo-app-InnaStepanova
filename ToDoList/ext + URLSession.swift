110f44947859050ecac7f7728abc7ccff0015cd0
InnaStepanova <inna-panichkina@yandex.ru>
2023-07-08 03:58:55 +0300

    DZ5

--- ToDoList/ext + URLSession.swift
+++ ToDoList/ext + URLSession.swift
@@ -4,29 +4,29 @@
 //
 //  Created by Лаванда on 04.07.2023.
 //
-
 import UIKit
 extension URLSession {
-        func dataTask(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
+    func dataTask(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
+
+        var task: URLSessionDataTask?
+
+        return try await withTaskCancellationHandler {
             try await withCheckedThrowingContinuation { continuation in
-                let task = self.dataTask(with: urlRequest) { data, response, error in
+                task = self.dataTask(with: urlRequest) { data, response, error in
                     if let error = error {
                         continuation.resume(throwing: error)
                     } else if let data = data, let response = response {
                         continuation.resume(returning: (data, response))
                     } else {
-                        let error = NSError(domain: "URLSessionDataTask Error", code: 0, userInfo: nil)
-                        continuation.resume(throwing: error)
+                        let unexpectedError = NSError(domain: "UnexpectedErrorDomain", code: 0, userInfo: nil)
+                        continuation.resume(throwing: unexpectedError)
                     }
                 }
-                task.resume()
-                
-                continuation.cancelHandler = {
-                    task.cancel()
-                }
+                task?.resume()
             }
+        } onCancel: { [weak task] in
+            task?.cancel()
         }
     }
-
-
+}
 
